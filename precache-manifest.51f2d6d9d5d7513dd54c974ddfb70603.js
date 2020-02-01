d2e8dab84b8ae8142627e36bdff0f539acb4d538
YishengSun <yisheng4@illinois.edu>
2020-02-01 16:03:30 -0600

    Updates

---
+++ precache-manifest.51f2d6d9d5d7513dd54c974ddfb70603.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ead1fcf201facc002e4d79b84d72ffc",
+    "url": "/Online-Store/index.html"
+  },
+  {
+    "revision": "650a187fc9201233b4e0",
+    "url": "/Online-Store/static/css/main.a64067ed.chunk.css"
+  },
+  {
+    "revision": "9b9bcbed9deb572fb73a",
+    "url": "/Online-Store/static/js/2.cb3207e8.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/Online-Store/static/js/2.cb3207e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "650a187fc9201233b4e0",
+    "url": "/Online-Store/static/js/main.b9a0bc95.chunk.js"
+  },
+  {
+    "revision": "7c1425f613232d6955fa",
+    "url": "/Online-Store/static/js/runtime-main.ea8261e6.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/Online-Store/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/Online-Store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
